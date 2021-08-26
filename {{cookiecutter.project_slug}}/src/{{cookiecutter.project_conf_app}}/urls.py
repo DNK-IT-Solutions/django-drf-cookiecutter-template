1955051b47bd19661d0cbfb0284d47b468643618
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-08-26 21:24:23 +0600

    Django project created  * Installed django framework and DRF. Added basic configurations for them.  * Installed and configured static code analyzers and tools for its formatting.  * Added configuration for code editors  * Created Makefile to install dependencies, run code analyzers and formatters, and tests

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_conf_app}}/urls.py
@@ -0,0 +1,42 @@
+"""{{cookiecutter.project_conf_app}} URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="{{ cookiecutter.project_slug }} API",
+        default_version="v1",
+        contact=openapi.Contact(email="{{ cookiecutter.author }}"),
+    ),
+    public=True,
+    permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
+)
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("users/", include("users.urls")),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("swagger<str:format>", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
