ed1b3e87129d72adcf2e977d6d95688b5058fb0e
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-08-26 21:34:30 +0600

    Add config for cookiecutter and cookiecutter hooks

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+echo -ne "Running with "
+
+python --version
+
+echo Creating and populating virtualenv..
+
+make install-deps
+
+cd src
+
+echo Collecting static assets...
+./manage.py collectstatic
+
+echo Running initial migrations...
+./manage.py migrate
+
+cd ../
+echo Running linters, vulnerability checking, type checking
+make analyze-source-code
+
+echo Running test...
+make test
+
+echo Done
