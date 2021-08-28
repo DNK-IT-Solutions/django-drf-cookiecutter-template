ee33c49624b6b5efe71961af98f02ff69f449139
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-08-28 22:57:16 +0600

    Add generation .env file in hooks:   Added generation of .env file using template parameters in the pre_gen_project hook, which is launched before the project template is rendered. The .env file adds the database configuration, Django's secret key, debug parameters, as well as a directory for static files, as well as the Sentry SDN if the Sentry is enabled (use_sentry = true)

---
+++ hooks/pre_gen_project.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+
+
+echo Generating .env file...
+
+echo SECRET_KEY="{{ cookiecutter.ENV_SECRET_KEY }}" > .env
+echo DEBUG="{{ cookiecutter.ENV_DEBUG }}" >> .env
+echo DATABASE_URL="{{ cookiecutter.ENV_DATABASE_URL }}" >> .env
+echo DJANGO_STATIC_ROOT="{{ cookiecutter.ENV_DJANGO_STATIC_ROOT }}" >> .env
+
+{% if cookiecutter.use_sentry == "yes" %}echo  SENTRY_DSN="{{ cookiecutter.ENV_SENTRY_DSN }}" >> .env {% endif %}
+
+echo Done
